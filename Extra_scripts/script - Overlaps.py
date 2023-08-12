fb03d72619769d308958494a8887d817171d12d3
sachaRfd <sacha.raffaud@outlook.com>
2023-08-12 22:05:57 +0200

    Quick fix

--- Extra_scripts/script - Overlaps.py
+++ Extra_scripts/script - Overlaps.py
@@ -2,23 +2,22 @@ import os
 import pymol
 
 # Start PyMOL and suppress its GUI
-pymol.finish_launching(['pymol', '-q'])
+pymol.finish_launching(["pymol", "-q"])
 
 # Set the background color to white
 pymol.cmd.bg_color("white")
 
 # Get a list of all directories in the current directory
-directory_list = [dir for dir in os.listdir('.') if os.path.isdir(dir)]
+directory_list = [dir for dir in os.listdir(".") if os.path.isdir(dir)]
 
 for directory in directory_list:
     # Change to the current directory
     os.chdir(directory)
 
     # Get a list of all .xyz files in the folder
-    file_list = [file for file in os.listdir('.') if file.startswith('sample')]
+    file_list = [file for file in os.listdir(".") if file.startswith("sample")]
 
     for file in file_list:
-
         # Load the XYZ file
         pymol.cmd.load(file, "my_structure")
 
@@ -39,7 +38,7 @@ for directory in directory_list:
         # # Center the view
         # pymol.cmd.center("all")
 
-        # Colour the true sample and the generated one: 
+        # Colour the true sample and the generated one:
         pymol.cmd.color("green", "my_structure and element C")
         pymol.cmd.color("grey", "true_samples and element C")
 
@@ -47,53 +46,48 @@ for directory in directory_list:
         # pymol.cmd.align("true_samples", "my_structure")
         pymol.cmd.align("my_structure", "true_samples")
 
-
         # Calculate RMSD
         rmsd = pymol.cmd.rms_cur("my_structure", "true_samples")
         # rmsd = 1
         # Save the overlap image with RMSE value
-        overlap_image_name = "overlap_{}_{}.png".format(os.path.splitext(file)[0], rmsd)
-        pymol.cmd.png(overlap_image_name, width=800, height=600, dpi=300, ray=1)
+        overlap_image_name = "overlap_{}_{}.png".format(
+            os.path.splitext(file)[0], rmsd
+        )  # noqa
+        pymol.cmd.png(overlap_image_name, width=800, height=600, dpi=300, ray=1)  # noqa
 
         # Clear the palet and everything else for next iteration
         pymol.cmd.reinitialize()
 
     # Go back to the parent directory
-    os.chdir('..')
+    os.chdir("..")
 
 for directory in directory_list:
     # Change to the current directory
     os.chdir(directory)
 
     # Get a list of all .xyz files in the folder
-    file_list = [file for file in os.listdir('.') if file.endswith('.xyz')]
+    file_list = [file for file in os.listdir(".") if file.endswith(".xyz")]
 
     # Set the color of carbon atoms to green
     pymol.cmd.color("green", "element C")
     pymol.cmd.show("sticks", "all")
 
-
     for file in file_list:
         # Load the XYZ file
         pymol.cmd.load(file, "my_structure")
-
-
 
         pymol.cmd.hide("everything", "my_structure")
 
         # Set the representation style for atoms
         pymol.cmd.show("sticks", "all")
 
-        
         pymol.cmd.color("green", "element C")
 
         pymol.cmd.show("sticks", "my_structure")  # Try again
 
-
         # Center the view
         pymol.cmd.center("all")
         pymol.cmd.show("sticks", "all")  # Try again
-
 
         # Adjust the viewport
         pymol.cmd.viewport(800, 600)
@@ -106,7 +100,7 @@ for directory in directory_list:
         pymol.cmd.reinitialize()
 
     # Go back to the parent directory
-    os.chdir('..')
+    os.chdir("..")
 
 # Quit PyMOL
 pymol.cmd.quit()
