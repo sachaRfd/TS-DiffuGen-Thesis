e497c80afef1e704e5375184f5c24d3079be73f2
sachaRfd <sacha.raffaud@outlook.com>
2023-08-29 16:55:02 +0200

    changing setup file to changed name

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ CD to root directory and run: python setup.py install
 
 
 setup(
-    name="DiffSach",
+    name="TS-DiffuGen",
     version="0.1.0",
     author="Sacha Raffaud",
     author_email="sacha.raffaud@outlook.com",
     description="Diffusion Model for Transition State Geometry Prediction",
-    url="https://github.com/schwallergroup/DiffSach",
+    url="https://github.com/schwallergroup/TS-DiffuGen",
     packages=find_packages(),  # Automatically find all packages in the project directory   # noqa
 )
