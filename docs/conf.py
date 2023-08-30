d732d5d23b83d1f7e53c618d233bb28186803e0f
sachaRfd <sacha.raffaud@outlook.com>
2023-08-30 19:59:07 +0200

    Trying documentation script

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+autodoc_mock_imports = ["TS_DiffuGen"]
+
+
+# Project information
+project = "TS-DiffuGen"
+author = "Sacha Raffaud"
+version = "1.0"
+release = "1.0.0"
+
+master_doc = "index"
+
+# General configuration
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+source_suffix = ".rst"
+exclude_patterns = ["_build"]
+
+
+# Options for LaTeX output
+latex_elements = {"extraclassoptions": "openany,oneside"}
+
+# Options for manual page output
+man_pages = [(master_doc, "myproject", "My Project Documentation", [author], 1)]  # noqa
+
+# Options for Texinfo output
+texinfo_documents = [
+    (
+        master_doc,
+        "MyProject",
+        "My Project Documentation",
+        author,
+        "MyProject",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
+]
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+TS-DiffuGen: Diffusion Model for Reaction Transition State optimisation
+=======================================================================
+
+This is the documentation for the TS-DiffuGen Package.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: src
+   :members: 
+   :inherited-members:
+   :undoc-members:
+   :show-inheritance:
+   :noindex:
