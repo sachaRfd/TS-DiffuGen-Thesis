3657f0d915b643cddface8c17dd2d879567d2302
sachaRfd <sacha.raffaud@outlook.com>
2023-08-30 21:47:24 +0200

    revert 2

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 
-sys.path.insert(0, os.path.abspath(os.sep.join((os.curdir, "../src"))))  # noqa
+sys.path.insert(0, os.path.abspath(".."))  # Adjust the path to the root directory
 
 
 extensions = [
